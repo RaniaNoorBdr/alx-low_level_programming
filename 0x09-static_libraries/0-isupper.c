16bac687d91a894006203f357d7b4022be491dab
ranianoor <rauniv08@gmail.com>
2023-04-10 10:08:17 -0700

    efs

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,17 +1,1 @@
-#include "main.h"
-
-/**
- * _isupper - uppercase letters
- * @c: char to check
- *
- * Return: 0 or 1
- */
-
-int _isupper(int c)
-{
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else
-		return (0);
-}
-
+0000000000000000 T _isupper
